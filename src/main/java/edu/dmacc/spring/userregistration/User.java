d7bd476ad24fd8958ef36984235e23d75f5fe945
jntfaz <35754069+jntfaz@users.noreply.github.com>
2018-03-26 19:07:43 -0500

    mvc project

---
+++ src/main/java/edu/dmacc/spring/userregistration/User.java
@@ -0,0 +1,136 @@
+package edu.dmacc.spring.userregistration;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String lastname;
+	private String password;
+	private String detail;
+	private String birthMonth;
+	private Gender gender;
+	private String country;
+	private boolean nonSmoking;
+
+
+	public User() {
+
+	
+	}
+	
+	public int getId() {
+
+		return id;
+
+	}
+
+	public void setId(int id) {
+
+		this.id = id;
+
+	}
+
+	public String getName() {
+
+		return name;
+
+	}
+
+	public void setName(String name) {
+
+		this.name = name;
+
+	}
+
+	public String getLastname() {
+
+		return lastname;
+
+	}
+
+	public void setLastname(String lastname) {
+
+		this.lastname = lastname;
+
+	}
+
+	public String getPassword() {
+
+		return password;
+
+	}
+
+	public void setPassword(String password) {
+
+		this.password = password;
+
+	}
+	public String getDetail() {
+
+		return detail;
+
+	}
+
+	public void setDetail(String detail) {
+
+		this.name = detail;
+
+	}
+
+	public String getBirthMonth() {
+
+		return birthMonth;
+
+	}
+	
+	public Gender getGender() {
+
+		return gender;
+
+	}
+
+	public void setGender(Gender gender) {
+
+		this.gender = gender;
+
+	}
+
+	public void setBirthMonth(String birthMonth) {
+
+		this.birthMonth = birthMonth;
+
+	}
+
+	public boolean getNonSmoking() {
+
+		return nonSmoking;
+
+	}
+
+	public void setNonSmoking(boolean nonSmoking) {
+
+		this.nonSmoking = nonSmoking;
+
+	}
+	public String getCountry() {
+
+		return country;
+
+	}
+
+	public void setCountry(String country) {
+
+		this.country = country;
+
+	}
+
+}
